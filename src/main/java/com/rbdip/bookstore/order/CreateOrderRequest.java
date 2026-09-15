690c8e5256204ac1c0d97da41d093ee715825bd5
Данила Солодовников <dans2005@yandex.ru>
2026-09-15 16:36:28 +0300

    lab1 v3

--- src/main/java/com/rbdip/bookstore/order/CreateOrderRequest.java
+++ src/main/java/com/rbdip/bookstore/order/CreateOrderRequest.java
@@ -2,7 +2,6 @@ package com.rbdip.bookstore.order;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 import java.util.List;
